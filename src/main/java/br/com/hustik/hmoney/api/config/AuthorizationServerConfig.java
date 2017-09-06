d5bbe272ee8c5b06a932aa2fb9e5cb6375a6f026
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2017-09-06 00:08:27 -0300

    Suporte a renovação do Access Token com Refresh Token

--- src/main/java/br/com/hustik/hmoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/br/com/hustik/hmoney/api/config/AuthorizationServerConfig.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer
@@ -25,25 +25,35 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .withClient("angular")
                 .secret("@ngul@r0")
                 .scopes("read", "write")
+                .authorizedGrantTypes("password", "refresh_token") // permite obter novo access_token através do refresh_token
+                .accessTokenValiditySeconds(20) // 20 segundos para testes com refresh token
+                .refreshTokenValiditySeconds(3600 * 24); // refresh_token válido por 1 dia
         /*
          * É possível definir scopes diferentes para clientes diferentes
          * Portanto, para utilizar os Scopes definidos aqui, é necessário realizar o devivo tratamento para que tenham efeito e façam sentido
          */
-                .authorizedGrantTypes("password")
-                .accessTokenValiditySeconds(1800); // 30 minutos
     }
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .tokenStore(tokenStore()) // É necessário armazenar o token em alguma lugar
+                .accessTokenConverter(accessTokenConverter())
+                .reuseRefreshTokens(false) // "Sempre que eu pedir um novo access_token usando refresh_token, um novo refresh_token será enviado"
                 .authenticationManager(authenticationManager);
         // o angular buscará o token aqui e depois retornará esse string para que seja possível acessar a API
     }
 
     @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("hustik");
+        return accessTokenConverter;
+    }
+
+    @Bean
     public TokenStore tokenStore() {
-        return new InMemoryTokenStore();
+        return new JwtTokenStore(accessTokenConverter());
     }
 
 }
