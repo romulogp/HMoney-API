70d87ffb34389b0b0b26dee7a50c8a005ac38c7b
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2017-09-09 00:30:43 -0300

    Adição das restrições de ROLES e SCOPES para Usuário e Lançamento

--- src/main/java/br/com/hustik/hmoney/api/resource/LancamentoResource.java
+++ src/main/java/br/com/hustik/hmoney/api/resource/LancamentoResource.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
@@ -39,11 +40,13 @@ public class LancamentoResource {
     private MessageSource messageSource;
 
     @GetMapping
-    private Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
+    public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         return lancamentoRepository.filtrar(lancamentoFilter, pageable);
     }
 
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
     public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
         Lancamento lancamentoRecuperado = lancamentoRepository.findOne(codigo);
         return lancamentoRecuperado == null
@@ -52,6 +55,7 @@ public class LancamentoResource {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
     public ResponseEntity<Lancamento> criar(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
         Lancamento lancamentoSalvo = lancamentoService.salvar(lancamento);
         eventPublisher.publishEvent(
@@ -60,6 +64,7 @@ public class LancamentoResource {
     }
 
     @DeleteMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_REMOVER_LANCAMENTO') and #oauth2.hasScope('write')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long codigo) {
         lancamentoRepository.delete(codigo);
--- src/main/java/br/com/hustik/hmoney/api/resource/PessoaResource.java
+++ src/main/java/br/com/hustik/hmoney/api/resource/PessoaResource.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -27,6 +28,7 @@ public class PessoaResource {
     private ApplicationEventPublisher eventPublisher;
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
     public ResponseEntity<Pessoa> criar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
         Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 
@@ -36,6 +38,7 @@ public class PessoaResource {
     }
 
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and #oauth2.hasScope('read')")
     public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo) {
         Pessoa pessoaRecuperada = pessoaRepository.findOne(codigo);
         return pessoaRecuperada != null
@@ -44,18 +47,21 @@ public class PessoaResource {
     }
 
     @DeleteMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_REMOVER_PESSOA') and #oauth2.hasScope('write')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long codigo) {
         pessoaRepository.delete(codigo);
     }
 
     @PutMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
     public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa novaPessoa) {
         Pessoa pessoaAtualizada = pessoaService.atualizar(codigo, novaPessoa);
         return ResponseEntity.ok(pessoaAtualizada);
     }
 
     @PatchMapping("/{codigo}/ativo")
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void atualizaPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
         pessoaService.atualizaPropriedadeAtivo(codigo, ativo);
